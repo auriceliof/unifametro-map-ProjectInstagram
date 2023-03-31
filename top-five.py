d2edadae74133694da9c1e2bb4e63e33bfa44732
AURICELIO FREITAS MOREIRA <1-2021221807>
2023-03-31 19:58:50 -0300

    Initial project

---
+++ top-five.py
@@ -0,0 +1,20 @@
+import instaloader;
+
+bot = instaloader.Instaloader()
+bot.login(user="php.arretado", password="")
+
+top_results = instaloader.TopSearchResults(bot.context, 'python')
+
+hashtags = top_results.get_hashtags()
+profiles = top_results.get_profiles()
+
+list_hashtags = []
+for hashtag in hashtags:
+    list_hashtags.append(hashtag.name)
+
+list_profiles = []
+for profile in profiles:
+    list_profiles.append(profile.full_name)
+
+print(list_hashtags)
+print(list_profiles)
